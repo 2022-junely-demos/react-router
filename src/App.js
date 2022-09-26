f01290ea5837b44f33ec80596c60224f081ccbbe
Julie Nisbet <jmnisbet@gmail.com>
2022-09-26 10:48:43 -0700

    add NotFound component and catch all route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route path="*" component={NotFound} />
       </Switch>
     </div>
   );
